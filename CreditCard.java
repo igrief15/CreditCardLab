b79e34dede41fca25f2e30074f5c2fa98b17a5f4
igrief15 <igrief15@punahou.edu>
2014-12-03 13:28:14 -1000

    credit card v0.9
    It has some degree of functionality

--- CreditCard.java
+++ CreditCard.java
@@ -13,50 +13,54 @@ public class CreditCard
     String oddstr = num;
     String evenstr = num;
     for (int i = 0; i < num.length(); i++){ //flips the input, 12345 becomes 54321
-     oddsum = oddsum + oddstr.substring(oddstr.length()-1,oddstr.length());
-     System.out.println(oddsum); //check, delet later
-     oddstr = oddstr.substring(0,oddstr.length()-1); 
+      oddsum = oddsum + oddstr.substring(oddstr.length()-1,oddstr.length());
+      System.out.println(oddsum); //check, delet later
+      oddstr = oddstr.substring(0,oddstr.length()-1); 
     }
     String tempstring = oddsum;
     oddsum = "";
-    for (int j = 0; j < tempstring.length(); j++){ 
+    for (int j = 0; j < tempstring.length()/2+1; j++){ 
       oddsum = oddsum + tempstring.substring(j*2,j*2+1); 
-      System.out.println(oddsum); //check, delete later
-      System.out.println(tempstring);
-      System.out.println("this is a check");
+    }
+    for (int b = 0; b < tempstring.length()/2; b++){
+      evensum = evensum + tempstring.substring(b*2+1, b*2+2);
     }
     
-      
-   /* int[] numar = new int[a];
-    for (int i = 0; i < num.length(); i++) {
-      numar[i] = Integer.valueOf(num.substring(i, i + 1)); //found string parsing stuff online
+    int[] oddar = new int[oddsum.length()];
+    int[] evenar = new int[evensum.length()];
+    for (int i = 0; i < oddsum.length(); i++) {
+      oddar[i] = Integer.valueOf(oddsum.substring(i, i + 1)); //found string parsing stuff online
     }
-    for (int i = 0; i < numar.length;i++)
+    for (int i = 0; i < evensum.length(); i++) {
+      evenar[i] = Integer.valueOf(evensum.substring(i, i + 1)); //found string parsing stuff online
+    }
+    int oddcheck = 0;
+    int evencheck = 0;
+    int totalcheck = 0;
+    for (int i = 0; i < oddar.length;i++)
     {
-      oddsum =+ numar[numar.length - (i + i*2)];
+      oddcheck = oddcheck + oddar[i];
     }
-    */
-    System.out.println(oddsum); //check, delete
-    return "cat";
-   
+    for (int i = 0; i < evenar.length;i++)
+    {
+      evencheck = evencheck + evenar[i];
+    }
+    totalcheck = (evencheck * 2) + oddcheck;
+    System.out.println(totalcheck);
+    if (totalcheck%10 == 0)
+    {
+      valid = true;
+      return "true";
+    }
+    else 
+    {
+      valid = false;
+      return "false";
+    }
     
     
     /*
-     for loop, int i  ; i < cats.length ; i++
-     oddsum =+ cats[cats.length - (i + i*2)] 
-     
-     something like that
-     
-     
-     int digits
-     for loop
-     digits = (cats[cats.length - (i*2)])*2
-     if digits > 9 
-     input into array again and add the values, digits takes that value
-     evensum =+ digits
-     else 
-     evensum =+ digits
-     
+     * 
      boolean luhn = false
      
      if evensum + oddsum % 10 == 0 
@@ -64,7 +68,7 @@ public class CreditCard
      
      */
     
-      /*
+    /*
      int number = Integer.parseInt(num); //converts string to an int somehow, looked it up online
      int[] numar;
      for (int i = 0; i<num.length();i++)
@@ -76,26 +80,26 @@ public class CreditCard
   }
   int findType(String cats)
   {
-   {
-     if (cats.substring(0) == "4")
-   return 2;
-   else if (cats.substring(0,2) == "51" || cats.substring(0,2) == "52" || cats.substring(0,2) == "53" ||cats.substring(0,2) == "54" ||cats.substring(0,2) == "55")
-   return 1;
-   else if (cats.substring(0,4)== "6011")
-   return 4;
-   else if (cats.substring(0,2) == "34" || cats.substring(0,2) == "37")
-   return 3;
-   else if (cats.substring(0,2) == "36" || cats.substring(0,2) == "38")
-   return 5;
-     else if (cats.substring(0,3) == "300" || cats.substring(0,3) == "301" || cats.substring(0,3) == "302" 
-   || cats.substring(0,3) == "303" || cats.substring(0,3) == "304" || cats.substring(0,3) == "305")
-   return 5;
-   else if (valid ==false)
-   return 0;
-   else 
-   return 0;
-   }
-  
+    {
+      if (cats.substring(0) == "4")
+      return 2;
+      else if (cats.substring(0,2) == "51" || cats.substring(0,2) == "52" || cats.substring(0,2) == "53" ||cats.substring(0,2) == "54" ||cats.substring(0,2) == "55")
+      return 1;
+      else if (cats.substring(0,4)== "6011")
+      return 4;
+      else if (cats.substring(0,2) == "34" || cats.substring(0,2) == "37")
+      return 3;
+      else if (cats.substring(0,2) == "36" || cats.substring(0,2) == "38")
+      return 5;
+      else if (cats.substring(0,3) == "300" || cats.substring(0,3) == "301" || cats.substring(0,3) == "302" 
+                 || cats.substring(0,3) == "303" || cats.substring(0,3) == "304" || cats.substring(0,3) == "305")
+      return 5;
+      else if (valid ==false)
+      return 0;
+      else 
+      return 0;
+    }
+    
   }
   
 }
